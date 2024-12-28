9c0dc8c7f6a31f77b210a30e991afa2ea2720abc
Isaac Bernat <isaac.bernat@gmail.com>
2024-12-28 17:41:08 +0100

    Add enrich_metadata.py

--- create_archive.py
+++ create_archive.py
@@ -1,5 +1,6 @@
 from fetch_images import fetch_images
 from fetch_metadata import create_metadata_csv
+from enrich_metadata import enrich_metadata_csv
 from image_to_pdf import create_pdf
 LATEST = 500
 
@@ -8,4 +9,5 @@ if __name__ == '__main__':
     print(f"Creating archive for up to day {LATEST}.")
     fetch_images(LATEST)
     create_metadata_csv(LATEST)
+    enrich_metadata_csv()
     create_pdf()
