329e23336d343b7f487810a1f3fdc47c69e08281
Isaac Bernat <isaac.bernat@gmail.com>
2025-02-18 11:09:30 +0100

    Add latest pdf version of the archive

--- create_archive.py
+++ create_archive.py
@@ -2,7 +2,7 @@ from fetch_images import fetch_images
 from fetch_metadata import create_metadata_csv
 from enrich_metadata import enrich_metadata_csv
 from image_to_pdf import create_pdf
-LATEST = 500
+LATEST = 556
 
 
 if __name__ == '__main__':
