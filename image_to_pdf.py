faf4c30272338d7022dbb9cf1f6d50088c67805a
Isaac Bernat <isaac.bernat@gmail.com>
2025-03-25 21:46:30 +0100

    refactor in preparation of create_video_page

--- image_to_pdf.py
+++ image_to_pdf.py
@@ -172,11 +172,16 @@ def create_canvas(output_pdf, size=A4):
     return c, x_pos, scaled_width
 
 
-def create_pdf_from_images(input_directory, pdf_dir, titles, image_files, size=A4, batch=100):
+def create_pdf_from_images(script_dir, titles, size=A4, batch=100, include_video=False):
+    image_dir = os.path.join(script_dir, "images")
+    image_files = sorted([f for f in os.listdir(image_dir) if f.endswith('.jpg')])
+    pdf_dir = os.path.join(script_dir, "pdf")
+    os.makedirs(pdf_dir, exist_ok=True)  # Create pdf directory if needed
+
     page_width, page_height = size
     for page_num, image_file in enumerate(image_files, 1):  # Process each image
         if page_num % batch == 1:
-            output_pdf = os.path.join(pdf_dir, f"basepaint_archive_{page_num}_to_{page_num+99}.pdf")
+            output_pdf = os.path.join(pdf_dir, f"basepaint_archive_{page_num}_to_{page_num+batch-1}.pdf")
             c, x_pos, scaled_width = create_canvas(output_pdf)
 
         day_num = int(image_file.split('.')[0])  # Extract day number (assuming XXXX.jpg)
@@ -184,7 +189,7 @@ def create_pdf_from_images(input_directory, pdf_dir, titles, image_files, size=A
             print(f"Processing image {day_num}/{len(image_files)}")
 
         draw_header(c, day_num, titles, x_pos, page_height, page_width)
-        image_path = os.path.join(input_directory, image_file)
+        image_path = os.path.join(image_dir, image_file)
         c.drawImage(image_path,
                    x_pos,  # center horizontally
                    page_height - scaled_width - 70,  # position below header
@@ -198,12 +203,13 @@ def create_pdf_from_images(input_directory, pdf_dir, titles, image_files, size=A
         draw_footer_line(c, 40, page_width, "Artwork generated collaboratively at  ", f"https://basepaint.xyz/canvas/{day_num}")
         draw_footer_line(c, 40 - 15, page_width, "Archive available at  ", "https://github.com/isaacbernat/basepaint")
         c.showPage()
-        if page_num % 100 == 0:
+
+        if page_num % batch == 0:
             c.save()
             print(f"saved {output_pdf}")
 
 
-def create_cover(input_directory, pdf_dir, size, image_files):
+def create_cover(image_dir, pdf_dir, size, image_files):
     print("Creating PDF cover...")
     output_pdf = os.path.join(pdf_dir, "basepaint_archive_000_cover.pdf")
     page_width, page_height = size
@@ -218,7 +224,7 @@ def create_cover(input_directory, pdf_dir, size, image_files):
     c.drawString(x_pos + 10, page_height - 105, subtitle)
     c.drawString(349, page_height - 105, f"From day #1 to #{len(image_files)}")
 
-    draw_mosaic(c, x_pos, page_height, image_files, input_directory)
+    draw_mosaic(c, x_pos, page_height, image_files, image_dir)
     draw_footer_line(c, 40, page_width, "Artwork generated collaboratively at  ", f"https://basepaint.xyz")
     draw_footer_line(c, 40 - 15, page_width, "Archive available at  ", "https://github.com/isaacbernat/basepaint")
 
@@ -226,21 +232,18 @@ def create_cover(input_directory, pdf_dir, size, image_files):
     c.save()
 
 
-def create_pdf(batch_size=100, add_cover=True):
+def create_pdf(batch_size=100, add_cover=True, include_video=False):
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    img_dir = os.path.join(script_dir, "images")
-    pdf_dir = os.path.join(script_dir, "pdf")
-    os.makedirs(pdf_dir, exist_ok=True)  # Create pdf directory if needed
     titles = load_titles('metadata.csv')
-    image_files = sorted([f for f in os.listdir(img_dir) if f.endswith('.jpg')])
-    size=A4
     load_fonts()
-    create_pdf_from_images(img_dir, pdf_dir, titles, image_files, size=size, batch=batch_size)
+    create_pdf_from_images(script_dir, titles, size=A4, batch=batch_size, include_video=include_video)
     if add_cover:
+        img_dir = os.path.join(script_dir, "images")
+        image_files = sorted([f for f in os.listdir(img_dir) if f.endswith('.jpg')])
         create_cover(
-            input_directory=img_dir,
+            image_dir=img_dir,
             pdf_dir=pdf_dir,
-            size=size,
+            size=A4,
             image_files=image_files,
         )
     print("Finish creating PDF.")
